e4928bc1e79365ab7c53b8943f3d6afdeaf0df09
Piotr Zduniak <piotr@zduniak.net>
2015-06-06 20:21:15 +0200

    Updated Dockerfile and removed Postfix

--- _postfix/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ">> starting postfix"
-service postfix start
-
-echo ">> printing the logs"
-touch /var/log/mail.log /var/log/mail.err /var/log/mail.warn
-chmod a+rw /var/log/mail.*
-tail -F /var/log/mail.*
