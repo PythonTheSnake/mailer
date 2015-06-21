2fbe8458beb78251a30d36616a9bc02feb967cec
Piotr Zduniak <piotr@zduniak.net>
2015-06-21 20:59:54 +0200

    Basics for using the new smtpd library

--- handler.go
+++ handler.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"crypto/tls"
+	"time"
 
 	"github.com/bitly/go-nsq"
 	r "github.com/dancannon/gorethink"
 	"github.com/getsentry/raven-go"
 	"github.com/lavab/go-spamc"
+	"github.com/lavab/smtpd"
 )
 
 type handler struct {
@@ -24,8 +26,55 @@ type handler struct {
 	Producer  *nsq.Producer
 	Raven     *raven.Client
 	Spam      *spamc.Client
+
+	SMTP *smtpd.Server
 }
 
 func (h *handler) ListenAndServe(addr string) error {
+	if h.SMTP == nil {
+		h.SMTP = &smtpd.Server{
+			Hostname:       h.Hostname,
+			WelcomeMessage: h.WelcomeMessage,
+
+			ReadTimeout:  time.Second * time.Duration(h.ReadTimeout),
+			WriteTimeout: time.Second * time.Duration(h.WriteTimeout),
+			DataTimeout:  time.Second * time.Duration(h.DataTimeout),
+
+			MaxConnections: h.MaxConnections,
+			MaxMessageSize: h.MaxMessageSize,
+			MaxRecipients:  h.MaxRecipients,
+
+			WrapperChain: []smtpd.Wrapper{
+				h,
+			},
+			RecipientChain: []smtpd.Recipient{
+				h,
+			},
+			DeliveryChain: []smtpd.Delivery{
+				h,
+			},
+
+			TLSConfig: h.TLSConfig,
+		}
+	}
+
 	return nil
+}
+
+func (h *handler) Wrap(x func()) func() {
+	return func() {
+		h.Raven.CapturePanic(x, nil)
+	}
+}
+
+func (h *handler) HandleRecipient(next func(conn *smtpd.Connection)) func(conn *smtpd.Connection) {
+	return func(conn *smtpd.Connection) {
+		next(conn)
+	}
+}
+
+func (h *handler) HandleDelivery(next func(conn *smtpd.Connection)) func(conn *smtpd.Connection) {
+	return func(conn *smtpd.Connection) {
+		next(conn)
+	}
 }
